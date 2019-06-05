4909755e4712a2e8ffd013cf7b2d085ccb0c8ae6
Allan Hall <bigal76126@gmail.com>
2019-06-05 13:47:06 -0400

    refactored code to concat the old list to update with new data

--- src/pages/Episodepage.jsx
+++ src/pages/Episodepage.jsx
@@ -5,9 +5,6 @@ import Homebutton from '../components/Homebutton'
 import { Link } from 'react-router-dom'
 
 export default function Homepage() {
-  const [episode, setEpisode] = useState('')
-  const [nextCall, setNextCall] = useState('')
-  const [episodeList2, setEpisodeList2] = useState([])
   const [episodeList, setEpisodeList] = useState([])
   useEffect(() => {
     Axios.get('https://rickandmortyapi.com/api/episode')
@@ -15,13 +12,12 @@ export default function Homepage() {
         console.log({ resp })
         console.log(resp.data.results)
         setEpisodeList(resp.data.results)
-        setNextCall(resp.data.info.next)
       })
       .then(
         Axios.get('https://rickandmortyapi.com/api/episode/?page=2').then(
           resp => {
             console.log({ resp })
-            setEpisodeList2(resp.data.results)
+            setEpisodeList(oldList => oldList.concat(resp.data.results))
           }
         )
       )
@@ -35,13 +31,6 @@ export default function Homepage() {
           return (
             <Link className="ind-links" to={`/episodes/${episode.id}`}>
               <div id="list-item">{episode.name}</div>
-            </Link>
-          )
-        })}
-        {episodeList2.map(episode2 => {
-          return (
-            <Link className="ind-links" to={`/episodes/${episode.id}`}>
-              <div id="list-item">{episode2.name}</div>
             </Link>
           )
         })}
